7ee615f59440c46d229724c6c1da357772b53a37
ThePotatoArchivist <josiahfu@gmail.com>
2025-10-14 21:28:34 -0700

    Initial mod

---
+++ src/main/java/archives/tater/classicmodernfarlands/RadiusCheck.java
@@ -0,0 +1,40 @@
+package archives.tater.classicmodernfarlands;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.util.dynamic.CodecHolder;
+import net.minecraft.world.gen.densityfunction.DensityFunction;
+
+import static java.lang.Math.max;
+
+public record RadiusCheck(int radius) implements DensityFunction.Base {
+    private static final Codec<Double> CONSTANT_RANGE = Codec.doubleRange(-1000000.0, 1000000.0);
+    public static final MapCodec<RadiusCheck> AXIS_VALUE_CODEC = RecordCodecBuilder.mapCodec(
+            instance -> instance.group(
+                            Codec.INT.fieldOf("radius").forGetter(RadiusCheck::radius)
+                    )
+                    .apply(instance, RadiusCheck::new)
+    );
+    public static final CodecHolder<RadiusCheck> CODEC_HOLDER = CodecHolder.of(AXIS_VALUE_CODEC);
+
+    @Override
+    public double sample(DensityFunction.NoisePos pos) {
+        return max(Math.abs(pos.blockX()), Math.abs(pos.blockZ())) >= radius ? 1.0 : 0.0;
+    }
+
+    @Override
+    public double minValue() {
+        return 0.0;
+    }
+
+    @Override
+    public double maxValue() {
+        return 1.0;
+    }
+
+    @Override
+    public CodecHolder<? extends DensityFunction> getCodecHolder() {
+        return CODEC_HOLDER;
+    }
+}
---
+++ src/main/java/archives/tater/classicmodernfarlands/mixin/OctavePerlinNoiseSamplerMixin.java
@@ -0,0 +1,19 @@
+package archives.tater.classicmodernfarlands.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import net.minecraft.util.math.noise.OctavePerlinNoiseSampler;
+
+@Mixin(OctavePerlinNoiseSampler.class)
+public class OctavePerlinNoiseSamplerMixin {
+    @ModifyReturnValue(
+            method = "maintainPrecision",
+            at = @At("RETURN")
+    )
+    private static double revertPrecisionFix(double original, @Local(argsOnly = true) double value) {
+        return value;
+    }
+}
