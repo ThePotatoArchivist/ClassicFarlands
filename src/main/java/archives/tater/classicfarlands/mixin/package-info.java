59dc8411b849afd50e5918dd05fb827697b4ad4a
ThePotatoArchivist <josiahfu@gmail.com>
2026-03-24 15:23:03 -0700

    Null specify

---
+++ src/main/java/archives/tater/classicfarlands/mixin/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package archives.tater.classicfarlands.mixin;
+
+import org.jspecify.annotations.NullMarked;
---
+++ src/main/java/archives/tater/classicfarlands/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package archives.tater.classicfarlands;
+
+import org.jspecify.annotations.NullMarked;
